809c16f167d6e5516f663cd19bdd6735fcb6ee56
AaronLai <zhifeng@webglsoft.com>
2023-07-13 10:45:12 +0800

    success

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>For test</h1>
+
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'store';
+}
